b08a3d18eb345dcca9af30a99c9dd88381243324
Vincent William Rodriguez <vincentwilliamr@gmail.com>
2025-04-16 15:27:16 +0800

    feat: convert to livewire

--- app/Livewire/TaskChild.php
+++ app/Livewire/TaskChild.php
@@ -12,6 +12,8 @@ class TaskChild extends Component
     public $showModal = false;
     public $isEditMode = false;
     public $showDel = false;
+    public $isComplete = false;
+    public $primaryKey;
 
     protected $listeners = ['showAdd', 'showEdit', 'showDel'];
 
@@ -33,6 +35,10 @@ class TaskChild extends Component
     //     $this->validateOnly($propertyName);
     // }
 
+    public function updatedItemStatus($value) {
+        $this->isComplete = $value === 'Complete';
+    }
+
     public function render() {
         return view('livewire.task-child');
     }
@@ -40,6 +46,31 @@ class TaskChild extends Component
     public function showAdd() {
         $this->resetForm();
         $this->showModal = true;
+    }
+
+    public function showEdit(int $id) {
+        $this->resetForm();
+        $data = Task::findOrFail($id);
+
+        $this->item = [
+            'id'        => $data->id,
+            'user_id'   => $data->user_id,
+            'name'      => $data->name,
+            'description'      => $data->description,
+            'status'                => $data->status,
+            'due_date'              => $data->due_date,
+            'completion_date'=> $data->completion_date,
+        ];
+
+        $this->isEditMode = true;
+        $this->showModal = true;
+    }
+
+
+    public function showDel($id) {
+        $this->resetForm();
+        $this->showDel = true;
+        $this->primaryKey = $id;
     }
 
     public function resetForm() {
@@ -50,17 +81,54 @@ class TaskChild extends Component
     public function saveTask() {
         $this->validate();
 
-        Task::create([
-            'user_id' => Auth::id(),
-            'name' => $this->item['name'],
-            'description' => $this->item['description'] ?? null,
-            'status' => $this->item['status'] ?? 'pending',
-            'due_date' => $this->item['due_date'] ?? null,
-            'completion_date' => null,
-        ]);
+        try {
 
-        session()->flash('success', 'Task created successfully');
+            if ($this->isEditMode) {
+                $data = Task::findOrFail($this->item['id']);
+                $data->update([
+                    'user_id' => Auth::id(),
+                    'name' => $this->item['name'],
+                    'description' => $this->item['description'] ?? null,
+                    'status' => $this->item['status'] ?? 'pending',
+                    'due_date' => $this->item['due_date'] ?? null,
+                    'completion_date' => null,
+                ]);
+
+                session()->flash('success', 'Task created successfully');
+
+            } else {
+
+                Task::create([
+                    'user_id' => Auth::id(),
+                    'name' => $this->item['name'],
+                    'description' => $this->item['description'] ?? null,
+                    'status' => $this->item['status'] ?? 'pending',
+                    'due_date' => $this->item['due_date'] ?? null,
+                    'completion_date' => null,
+                ]);
+
+                session()->flash('success', 'Task updated successfully');
+            }
+
+        } catch (\Exception $e) {
+            session()->flash('success', 'An error has occurred:'. $e);
+        }
+
+
+
         $this->showModal = false;
+        return redirect()->route('tasks.index');
+    }
+
+    public function deleteTask() {
+        try {
+            $data = Task::findOrFail($this->primaryKey);
+            $data->delete();
+            session()->flash('success', 'Task deleted successfully');
+        } catch (\Throwable $th) {
+            session()->flash('success', 'An error has occurred:'. $e);
+        }
+
         return redirect()->route('tasks.index');
     }
 }
--- resources/views/livewire/task-child.blade.php
+++ resources/views/livewire/task-child.blade.php
@@ -2,7 +2,7 @@
     @if ($showModal)
         <x-dialog-modal>
             <x-slot name="title">
-                Create Task
+                {{($isEditMode) ? 'Edit Task' : 'Create Task'}}
             </x-slot>
 
             <x-slot name="content">
@@ -22,7 +22,7 @@
                         </div>
                         <div class="col-span-6">
                             <x-label for="status" value="Status" />
-                            <select name="status" id="status" wire:model="item.status" class="w-full rounded-md block">
+                            <select name="status" id="status" wire:model.live="item.status" class="w-full rounded-md block">
                                 <option value="" selected>Select Status</option>
                                 <option value="Pending">Pending</option>
                                 <option value="In Progress">In Progress</option>
@@ -37,13 +37,40 @@
                                 wire:model="item.due_date" />
                             @error('item.due_date') <span class="text-red-500">{{ $message }}</span> @enderror
                         </div>
+
+                        @if ($isEditMode && $isComplete)
+                            <div class="col-span-6">
+                                <x-label for="due_date" value="Completion Date" />
+                                <x-input id="due_date" type="date" class="w-full rounded-md block"
+                                    wire:model="item.due_date" />
+                            </div>
+                        @endif
                     </div>
                 </div>
             </x-slot>
 
             <x-slot name="footer">
                 <x-button wire:click.prevent="$set('showModal', false)" mode="cancel" class="mx-4">Cancel</x-button>
-                <x-button wire:click="saveTask" mode="confirm" wire:loading.attr="disabled">Add</x-button>
+                <x-button wire:click="saveTask" mode="confirm" wire:loading.attr="disabled">{{ $isEditMode ? 'Update' : 'Add'}}</x-button>
+            </x-slot>
+
+        </x-dialog-modal>
+    @endif
+
+
+    @if ($showDel)
+        <x-dialog-modal>
+            <x-slot name="title">Delete Record</x-slot>
+
+            <x-slot name="content">
+                <div class="max-w-full w-full">
+                    <p class="text-center">Are you sure you want to delete this record?</p>
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <x-button wire:click.prevent="$set('showDel', false)" mode="cancel" class="mx-4">Cancel</x-button>
+                <x-button wire:click="deleteTask" mode="confirm" wire:loading.attr="disabled">Delete</x-button>
             </x-slot>
 
         </x-dialog-modal>
